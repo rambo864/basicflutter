3c2590ea93383c74f8a42ae4fa2fe54139badf54
rambo864 <72152045+rambo864@users.noreply.github.com>
2022-10-08 14:02:24 +0530

    Create intro.dart

---
+++ intro.dart
@@ -0,0 +1,14 @@
+void main() async {
+  // Read some data.
+  final fileData = await _readFileAsync();
+  final jsonData = jsonDecode(fileData);
+
+  // Use that data.
+  print('Number of JSON keys: ${jsonData.length}');
+}
+
+Future<String> _readFileAsync() async {
+  final file = File(filename);
+  final contents = await file.readAsString();
+  return contents.trim();
+}
